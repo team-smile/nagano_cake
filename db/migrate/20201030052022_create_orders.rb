d031f62438f431517bb267916230124903775fed
EC2 Default User <ec2-user@ip-172-31-22-206.ec2.internal>
2020-10-31 08:38:51 +0000

    [Fix] migration

--- db/migrate/20201030052022_create_orders.rb
+++ db/migrate/20201030052022_create_orders.rb
@@ -1,14 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-      t.integer :customer_id
-      t.string :postal_code
-      t.string :address
-      t.string :name
-      t.integer :postage
-      t.integer :total_payment
-      t.integer :payment_method
-      t.integer :status
+      t.integer :customer_id, null: false
+      t.string :postal_code, null: false
+      t.string :address, null: false
+      t.string :name, null: false
+      t.integer :postage, null: false
+      t.integer :total_payment, null: false
+      t.integer :payment_method, default:0, null: false
+      t.integer :status, default:0, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,10 @@
 ActiveRecord::Schema.define(version: 20201030052227) do
 
   create_table "addresses", force: :cascade do |t|
-    t.integer  "customer_id"
-    t.string   "postal_code"
-    t.string   "address"
-    t.string   "name"
+    t.integer  "customer_id", null: false
+    t.string   "postal_code", null: false
+    t.string   "address",     null: false
+    t.string   "name",        null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
@@ -34,74 +34,72 @@ ActiveRecord::Schema.define(version: 20201030052227) do
   end
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer  "item_id"
-    t.integer  "customer_id"
-    t.integer  "amount"
+    t.integer  "item_id",     null: false
+    t.integer  "customer_id", null: false
+    t.integer  "amount",      null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
   create_table "customers", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string   "last_name"
-    t.string   "string"
-    t.string   "first_name"
-    t.string   "last_name_kana"
-    t.string   "first_name_kana"
-    t.string   "postal_code"
-    t.string   "address"
-    t.string   "telphone_number"
-    t.string   "is_deleted"
-    t.string   "boolean"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string   "last_name",                              null: false
+    t.string   "first_name",                             null: false
+    t.string   "last_name_kana",                         null: false
+    t.string   "first_name_kana",                        null: false
+    t.string   "postal_code",                            null: false
+    t.string   "address",                                null: false
+    t.string   "telphone_number",                        null: false
+    t.boolean  "is_deleted",             default: false, null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
-    t.integer  "name"
-    t.boolean  "is_active"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",                      null: false
+    t.boolean  "is_active",  default: true, null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   create_table "items", force: :cascade do |t|
-    t.integer  "genre_id"
-    t.string   "name"
-    t.string   "image_id"
-    t.text     "introduction"
-    t.integer  "price"
-    t.boolean  "is_active"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.integer  "genre_id",                    null: false
+    t.string   "name",                        null: false
+    t.string   "image_id",                    null: false
+    t.text     "introduction",                null: false
+    t.integer  "price",                       null: false
+    t.boolean  "is_active",    default: true, null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   create_table "order_details", force: :cascade do |t|
-    t.integer  "item_id"
-    t.integer  "order_id"
-    t.integer  "price"
-    t.integer  "amount"
-    t.integer  "making_status"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.integer  "item_id",                   null: false
+    t.integer  "order_id",                  null: false
+    t.integer  "price",                     null: false
+    t.integer  "amount",                    null: false
+    t.integer  "making_status", default: 0, null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer  "customer_id"
-    t.string   "postal_code"
-    t.string   "address"
-    t.string   "name"
-    t.integer  "postage"
-    t.integer  "total_payment"
-    t.integer  "payment_method"
-    t.integer  "status"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.integer  "customer_id",                null: false
+    t.string   "postal_code",                null: false
+    t.string   "address",                    null: false
+    t.string   "name",                       null: false
+    t.integer  "postage",                    null: false
+    t.integer  "total_payment",              null: false
+    t.integer  "payment_method", default: 0, null: false
+    t.integer  "status",         default: 0, null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
