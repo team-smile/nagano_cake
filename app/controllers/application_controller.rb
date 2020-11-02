a71e9501faf04d55406f7e33440e570bcbe96508
EC2 Default User <ec2-user@ip-172-31-22-206.ec2.internal>
2020-11-02 23:36:37 +0000

    [Fix] view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_cart
+
+  def current_cart
+    if session[:cart_item_id]
+      @cart_item = CartItem.find(session[:cart_item_id])
+    else
+      @cart_item = CartItem.create
+      session[:cart_item_id] = @cart_item.id
+    end
+  end
 end
--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,16 +2,24 @@ class Public::CartItemsController < ApplicationController
   before_action :authenticate_user!
   
   def update
+    @cart_item.update(amount: params[:amount].to_i)
+    redirect_to current_cart
   end
 
   def index
-    @cart_items = CartItem.all
+    @cart_items = current_cart.cart_items
   end
 
+
   def create
+    @cart_item = current_user.cart_items.build(cart_item_params)
+    @cart_item.save
+    redirect_to current_cart
   end
 
   def destroy
+    @cart_item.destroy
+    redirect_to current_cart
   end
 
   def destroy_all
