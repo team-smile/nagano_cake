0cff6831ae6742e7b21f8d32960ecf12c4befad0
EC2 Default User <ec2-user@ip-172-31-26-34.ec2.internal>
2020-11-02 14:16:30 +0000

    [Add]genre index/edit/update/create

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
     root "homes#top"
     get "about" => "homes#about"
     resources :items, only: [:index, :show]
-    resources :genres
     resources :end_users, only: [:show, :edit, :update]
     get "end_users/unsubscribe" => "end_users#unsubscribe"
     patch "end_users/withdraw" => "end_users#withdraw"
@@ -31,7 +30,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get "admin" => "admin#homes"
     resources :items, except: [:destroy]
-    resources :genres, except: [:new, :show, :destroy]
+    resources :genres, only: [:edit, :update, :create, :index]
     resources :end_users, except: [:new, :create, :destroy]
     resources :orders, only: [:index, :show, :update] do
       patch "order_details/:id" => "order_details#update"
