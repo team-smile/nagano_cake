a0a122a2e2d1a5dfd0ebff5ad1bd94cfc746a10b
EC2 Default User <ec2-user@ip-172-31-22-206.ec2.internal>
2020-11-04 10:19:55 +0000

    [Add] controller

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,45 +1,48 @@
 class Public::CartItemsController < ApplicationController
+  # before_action :setup_cart_item!, only: [:create, :update, :destroy]
 
-
-  def update
-    @cart_item.update(amount: params[:amount].to_i)
-    redirect_to current_cart
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    # @cart_item.customer_id = current_customer.id
+    @cart_item.customer_id = 1
+    
+    # current_end_user.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+    if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
+      @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item.amount += params[:cart_item][:amount].to_i
+      @cart_item.update
+    else
+      @cart_item.save
+    end
+    redirect_to :cart_items
   end
 
   def index
     @cart_items = CartItem.all
+    # @cart_items = current_customer.cart_item
   end
 
-
-  def create
-    # @user = current_user
-
-    # @oreder = Order.new
-    # @order.user_id = current_user.id
-    # @order.save!
-
-    # @user.cart_items.each do |cart_item|
-
-    @cart_item = current_user.cart_items.build(cart_item_params)
-    @cart_items = current_user.cart_items.all
-    @cart_items.each do |cart_item|
-      if cart_item.item_id == @cart_item.item_id
-        new_amount = cart_item.amount + @cart_item.amount
-        cart_item.update_attribute(:amount, new_amount)
-        @cart_item.delete
-      end
-    end
-    @cart_item.save
-    redirect_to :cart_items
+  def update
+    @cart_item.update(amount: params[:amount].to_i)
+    redirect_to current_cart_item
   end
 
   def destroy
     @cart_item.destroy
-    redirect_to current_cart
+    redirect_to current_cart_item
   end
 
   def destroy_all
     @user = current_user
     @user.cart_items.delete_all
   end
+
+  private
+
+   def cart_item_params
+    params.require(:cart_item).permit(:amount, :item_id)
+   end
+  # def setup_cart_item!
+  #   @cart_item = current_cart_item.find_by(item_id: params[:item_id])
+  # end
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,20 @@
 class Public::OrdersController < ApplicationController
 
+  def create
+    @order = current_user.orders.new(order_params)
+    @order.save
+    @cart_items = current_user.cart_items.all
+      @cart_items.each do |cart_item|
+        @order_items = @order.cart_items.new
+        @order_items.item_id = cart_item.item.id
+        @order_items.name = cart_item.item.name
+        @order_items.price = cart_item.item.price
+        @order_items.amount = cart_item.amount
+        @order_items.save
+        current_user.cart_items.destroy_all
+      end
+  end
+
   def new
   end
 
@@ -7,9 +22,6 @@ class Public::OrdersController < ApplicationController
   end
 
   def complete
-  end
-
-  def create
   end
 
   def index
