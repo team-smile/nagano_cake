11decc97a1fc600ec82369482a0bbd9183f6fd09
EC2 Default User <ec2-user@ip-172-31-57-66.ec2.internal>
2020-11-05 13:25:18 +0000

    [Fix] ログイン状態でサインアップ画面を表示した時の動き

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters,if: :devise_controller?
   protect_from_forgery with: :exception
-
+  def after_sign_up_path_for(resource)
+    items_path(currnt_customer)
+  end
+  def after_sign_in_path_for(resource)
+    items_path(current_customer)
+  end
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [
