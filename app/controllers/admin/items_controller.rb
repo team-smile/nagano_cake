3c682a2599812ce8ce463e9e9d76724fd37c24ef
EC2 Default User <ec2-user@ip-172-31-26-34.ec2.internal>
2020-11-05 07:39:59 +0000

    [Add]item show/edit/update etc.

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,18 +9,30 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item_params)
-    item.save
-    redirect_to admin_items_path
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      render "new"
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
+    @tax_price = (@item.price * 1.1).round
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item)
+    else
+      render "edit"
+    end
   end
 
   private
