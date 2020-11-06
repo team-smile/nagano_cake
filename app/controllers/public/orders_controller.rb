c79b9aa9554cd132d387f5668ccfaa742b3df23b
EC2 Default User <ec2-user@ip-172-31-22-206.ec2.internal>
2020-11-06 02:20:35 +0000

    [Add] view

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,34 +1,34 @@
 class Public::OrdersController < ApplicationController
 
-  def create
-    @order = current_user.orders.new(order_params)
-    @order.save
-    @cart_items = current_user.cart_items.all
-      @cart_items.each do |cart_item|
-        @order_items = @order.cart_items.new
-        @order_items.item_id = cart_item.item.id
-        @order_items.name = cart_item.item.name
-        @order_items.price = cart_item.item.price
-        @order_items.amount = cart_item.amount
-        @order_items.save
-        current_user.cart_items.destroy_all
-      end
-  end
+  # def create
+  #   @order = current_customer.orders.new(order_params)
+  #   @order.save
+  #   @cart_items = current_customer.cart_items.all
+  #     @cart_items.each do |cart_item|
+  #       @order_items = @order.cart_items.new
+  #       @order_items.item_id = cart_item.item.id
+  #       @order_items.name = cart_item.item.name
+  #       @order_items.price = cart_item.item.price
+  #       @order_items.amount = cart_item.amount
+  #       @order_items.save
+  #       current_customer.cart_items.destroy_all
+  #     end
+  # end
 
-  def new
-    @order = Order.new
-    # @order.customer_id = current_customer.id
-  end
+  # def new
+  #   @order = Order.new
+  #   # @customer = current_customer
+  # end
 
-  def confirm
-  end
+  # def confirm
+  # end
 
-  def complete
-  end
+  # def complete
+  # end
 
-  def index
-    @orders = current_customer.cart_item
-  end
+  # def index
+  #   @orders = current_customer.cart_item
+  # end
 
   def show
   end
