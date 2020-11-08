c09d781506f1b02c50a11faf22fe1e58da0e7044
EC2 Default User <ec2-user@ip-172-31-26-34.ec2.internal>
2020-11-08 05:34:31 +0000

    [Fix]error message

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
-  validates :genre_id, :name, :image, :introduction, :price, presence: true
+  validates :genre_id, :name, :image, :introduction, presence: true
   validates :is_active, inclusion: [true, false]
   validates :price, numericality: {only_integer: true}
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
-  belongs_to :genre
+  belongs_to :genre, optional: true
   attachment :image
 end
