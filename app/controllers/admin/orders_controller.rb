6542d7bcb0c110299133a7855e04ba18f4ec70df
EC2 Default User <ec2-user@ip-172-31-80-206.ec2.internal>
2020-11-10 22:46:27 +0000

    [Update]3-4

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -11,12 +11,24 @@ class Admin::OrdersController < ApplicationController
     @order_details.each do |order_detail|
       @sum += order_detail.price*order_detail.amount
     end
+    @order.postage = 800
   end
 
   def update
     @order = Order.find(params[:id])
+    binding.pry
     if @order.update(order_params)
+      if @order.status == "入金確認"
+        @order_detail = OrderDetail.find(15)
+        @order_detail.update(making_status:"制作待ち")
+        redirect_to admin_order_path(@order)
+      elsif @order.status == "制作中"
+        @orders = Order.where(order_id: @order.id)
+        @orders.update(status:"制作中")
+        redirect_to admin_order_path(@order)
+      else
       redirect_to admin_order_path(@order)
+      end
     else
       @order = Order.find(params[:id])
       @order_details = @order.order_details
