0272c20a4400da3ac571323b67ca310da91c89c0
EC2 Default User <ec2-user@ip-172-31-57-66.ec2.internal>
2020-11-12 04:03:56 +0000

    Customer 編集・更新機能

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,11 +1,19 @@
 class Public::EndUsersController < ApplicationController
+  before_action :authenticate_customer!
   def show
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+  	if @customer.update(customer_params)
+  	   redirect_to end_users_my_page_path(@customer.id), notice: "You have updated customer successfully."
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
@@ -13,4 +21,9 @@ class Public::EndUsersController < ApplicationController
 
   def withdraw
   end
+
+  private
+    def customer_params
+  	  params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telphone_number)
+    end
 end
