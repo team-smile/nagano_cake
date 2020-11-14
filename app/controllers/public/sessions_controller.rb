c5858c37d635f1379867352c2f6320b99f55e30f
EC2 Default User <ec2-user@ip-172-31-80-206.ec2.internal>
2020-11-14 04:26:08 +0000

    [update]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -4,7 +4,7 @@ class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
    def after_sign_in_path_for(resource)
-    items_path(current_customer)
+    root_path
    end
 
   # GET /resource/sign_in
